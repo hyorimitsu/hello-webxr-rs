d936c3ce401f8b9cdba370d7d00606e61eab21c4
Hiroki Yorimitsu <yorimitsu@supinf.co.jp>
2022-03-06 02:43:48 +0900

    move: index.html from `app-web` to `app-web/public`

--- app-web/webpack.config.js
+++ app-web/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html'
+            template: 'public/index.html'
         }),
         new WasmPackPlugin({
             crateDirectory: path.resolve(__dirname, ".")
